fb13ef087d95707621716307712c69bf22c24785
MD. AREFUL ISLAM <32135229+arifislam007@users.noreply.github.com>
2023-09-09 17:42:25 +0600

    Update StartApplication.java

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -14,7 +14,7 @@ public class StartApplication {
     public String index(final Model model) {
         model.addAttribute("title", "Yes, CICD for Java Application has been deployed using docker");
         model.addAttribute("msg", "This application is deployed on docker with CICD Git>Jenkins>docker hub>docker by Md Areful Islam");
-        model.addAttribute("msg", "This is another line for Automating Build and deploye with CiCD");
+        model.addAttribute("msg1", "This is another line for Automating Build and deploye with CiCD");
         return "index";
     }
 
